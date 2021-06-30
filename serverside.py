827704cc834a4ba18849a1abd71f898f4230bdab
rozakcloud <rozakcloud@gmail.com>
2021-07-01 01:55:02 +0700

    finish gui for client chat

--- serverside.py
+++ serverside.py
@@ -32,6 +32,7 @@ def handle(client):
             client.close()
             nickname = nicknames[index]
             nicknames.remove(nickname)
+            break
 
 #receive untuk accept connection baru/client baru 
 def receive():
@@ -41,14 +42,17 @@ def receive():
 
         #untuk nicknames
         client.send("NICK".encode('utf-8'))
-        nickname = client.receive(1024)
+        nickname = client.recv(1024)
 
         nicknames.append(nickname)
         clients.append(client)
 
         print(f"Nickname dari client adalah {nickname}!")
-        broadcast(f"{nickname} ter-Konek dalam server!\n".encode('utf-8'))
-        client.send("Terkonek dalam server".encode('utf-8'))
+        broadcast(f"{nickname} tersambung dalam server!\n".encode('utf-8'))
+        client.send("Terkonek dalam server \n".encode('utf-8'))
 
         thread = threading.Thread(target=handle, args = (client,))
         thread.start()
+
+print("Server running")
+receive()
