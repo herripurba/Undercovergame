bba5a72ae0e48d739dd9c28512f07966f877fdce
NaufalRafi-hub <rafi.harahaps@gmail.com>
2021-06-30 20:28:04 +0700

    adding some of the functions

--- serverside.py
+++ serverside.py
@@ -15,7 +15,40 @@ nicknames = []
 #function
 
 #broadcast untuk ngirim pesen ke semua client
-
-#receive untuk accept connection baru/client baru 
+def broadcast(message):
+    for client in clients:
+        client.send(message)
 
 #handle untuk handle connection
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"{nicknames[clients.index(client)]} says {message}")
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+
+#receive untuk accept connection baru/client baru 
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Konek dengan {str(address)}!")
+
+        #untuk nicknames
+        client.send("NICK".encode('utf-8'))
+        nickname = client.receive(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname dari client adalah {nickname}!")
+        broadcast(f"{nickname} ter-Konek dalam server!\n".encode('utf-8'))
+        client.send("Terkonek dalam server".encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args = (client,))
+        thread.start()
